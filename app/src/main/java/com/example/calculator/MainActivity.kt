2963ec5ef41c4f9065c557c5c8d7fe87b31397cd
Anton Tymoshchuk <anton.tymoshchuk.personal@gmail.com>
2023-12-02 17:01:59 +0200

    Created method "numberButtonClick"

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,16 +2,37 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+enum class OperationType {
+    DIVIDE, MULTIPLY, MINUS, PLUS, NONE
+}
 
 class MainActivity : AppCompatActivity() {
+    private var numberA = 0.0
+    private var operationType = OperationType.NONE
+    private var numberB = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun numberButtonClick(view: View) {}
+    fun numberButtonClick(view: View) {
+        val screenTextView: TextView = findViewById<TextView>(R.id.screenTextView)
+        if (view is AppCompatButton) {
+            if (view.text.equals(getString(R.string.point))) {
+                if (!screenTextView.text.contains(getString(R.string.point)))
+                    screenTextView.append(getString(R.string.point))
+            } else if (screenTextView.text.equals(getString(R.string.zero))) {
+                screenTextView.text = view.text
+            } else {
+                screenTextView.append(view.text)
+            }
+        }
+    }
 
     fun operatorButtonClick(view: View) {}
 }

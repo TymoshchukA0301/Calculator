ae45e172cc3fc50c9649947e2d24dc32ad596f15
Anton Tymoshchuk <anton.tymoshchuk.personal@gmail.com>
2023-12-02 18:50:16 +0200

    Created method "operatorButtonClick"

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -34,5 +35,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun operatorButtonClick(view: View) {}
+    fun operatorButtonClick(view: View) {
+        val screenTextView: TextView = findViewById<TextView>(R.id.screenTextView)
+        if (view is AppCompatButton) {
+            if (!view.text.equals(getString(R.string.equals))) {
+                numberA = screenTextView.text.toString().toDouble()
+                screenTextView.text = getString(R.string.zero)
+                if (view.text.equals(getString(R.string.divide)))
+                    operationType = OperationType.DIVIDE
+                else if (view.text.equals(getString(R.string.multiply)))
+                    operationType = OperationType.MULTIPLY
+                else if (view.text.equals(getString(R.string.minus)))
+                    operationType = OperationType.MINUS
+                else if (view.text.equals(getString(R.string.plus)))
+                    operationType = OperationType.PLUS
+            } else if (view.text.equals(getString(R.string.equals))) {
+                if (operationType == OperationType.NONE)
+                    return
+                numberB = screenTextView.text.toString().toDouble()
+                var operationResult = 0.0
+                if (operationType == OperationType.DIVIDE)
+                    operationResult = numberA / numberB
+                else if (operationType == OperationType.MULTIPLY)
+                    operationResult = numberA * numberB
+                else if (operationType == OperationType.MINUS)
+                    operationResult = numberA - numberB
+                else if (operationType == OperationType.PLUS)
+                    operationResult = numberA + numberB
+                Log.d("my_tag", "numberA = $numberA")
+                Log.d("my_tag", "operationType = $operationType")
+                Log.d("my_tag", "numberB = $numberB")
+                screenTextView.text = operationResult.toString()
+            }
+        }
+    }
 }

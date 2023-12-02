ca9c08af9211b24e9a6eeffec6810de1b8caa52c
Anton Tymoshchuk <anton.tymoshchuk.personal@gmail.com>
2023-12-02 12:49:45 +0200

    Create methods for number and operator buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberButtonClick(view: View) {}
+
+    fun operatorButtonClick(view: View) {}
 }
